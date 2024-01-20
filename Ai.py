e15d54b4e4f4af7df1dd4e91fae0c3c921a65abb
Sakar Giri <sakargiri365@gmail.com>
2024-01-20 18:01:27 +0545

    AI integration

--- Ai.py
+++ Ai.py
@@ -1,4 +1,4 @@
-import replicate
+from bardapi import BardCookies
 import json
 import os
 
@@ -8,18 +8,15 @@ with open("API_KEY.json","r") as api_dict:
 #Set the REPLICATE_API_TOKEN environment variable
 os.environ["REPLICATE_API_TOKEN"] = api["key"]
 
+with open("API_KEY.json","r") as api_dict:
+    api = json.load(api_dict)
+
 def generate(prompt:str):
-    output = replicate.run(
-        "meta/llama-2-70b-chat:02e509c789964a7ea8736978a43525956ef40397be9033abf9fd2badfe68c9e3",
-        input={
-            "prompt": api["pre-prompt"] + prompt.capitalize(),
-            "temperature":0.1,"top_p":0.9,"max_length":512,
-        }
-    )
-
-    response = ''
-
-    for i in output:
-        response += i
-    print(prompt,response)    
-    return response
+    token = api["key"]
+    cookie_dict = {
+        "__Secure-1PSID":api["key"],
+        "__Secure-1PSIDTS":api["TSID"],
+        "__Secure-1PSIDCC":api["CCID"],
+    }
+    bard = BardCookies(cookie_dict=cookie_dict)
+    return bard.get_answer(f"{api['pre-prompt']} {prompt}")['content']
