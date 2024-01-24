47c597f8f119162a1227bb4e7455f4bd24bb68ac
Sakar Giri <sakargiri365@gmail.com>
2024-01-24 23:17:15 +0545

    stable ai integrated

--- Ai.py
+++ Ai.py
@@ -1,26 +1,18 @@
-from bardapi import BardCookies
 import json
-import os
+import google.generativeai as genai
 
 with open("API_KEY.json","r") as api_dict:
-    api = json.load(api_dict)
+    API = json.load(api_dict)
 
-#Set the REPLICATE_API_TOKEN environment variable
-os.environ["REPLICATE_API_TOKEN"] = api["key"]
 
-with open("API_KEY.json","r") as api_dict:
-    api = json.load(api_dict)
-
-def generate(prompt:str):
-    token = api["key"]
-    cookie_dict = {
-        "__Secure-1PSID":api["key"],
-        "__Secure-1PSIDTS":api["TSID"],
-        "__Secure-1PSIDCC":api["CCID"],
-    }
+class Assistant:
     try:
-        bard = BardCookies(cookie_dict=cookie_dict)
+        genai.configure(api_key=API['key'])
+        model = genai.GenerativeModel('gemini-pro')
     except Exception:
-        return "API key needs to be reinitiallized. Please wait untill the keys are set accordingly."
-    else:
-        return bard.get_answer(f"{api['pre-prompt']} \"{prompt.upper()}\"")['content']
+        pass
+
+    def generate(prompt:str):
+        try:response = Assistant.model.generate_content(f"{API['pre-prompt']} {prompt}")
+        except Exception:return "Request handeling overloaded. Please wait until new key is initilized"
+        else:return response.text
