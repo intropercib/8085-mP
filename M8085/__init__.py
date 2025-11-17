de13c8239717be421b6e97dc0e534eec198b3763
Sakar Giri <sakargiri365@gmail.com>
2025-11-17 16:39:29 +0545

    execution loop implementation

--- M8085/__init__.py
+++ M8085/__init__.py
@@ -1,31 +1,42 @@
-from ._parser import Parser
-from ._utils import decode, encode
+from ._parser import Parser, Message
+from ._utils import decode, encode, INSTRUCTION
 from ._arithmetic import Arithmetic
 from ._data import Data
 from ._logical import Logical
 from ._peripheral import Peripheral
 from ._stack import Stack
+from ._branch import Branch
 from ._timing import TimingDiagram
-from ._memory import Memory, Register, Flag
+from ._memory import Memory, Register, Flag, Assembler, _STACK
+from .logs import setup_logger, info
+
+RUNTIME = 1000
+setup_logger()
 
 class Processor:
 
-    def __init__(self,instruction:str):
+    def __init__(self,input:str):
 
         self.__arithmetic = Arithmetic()
         self.__data = Data()
         self.__logical = Logical()
         self.__peripheral = Peripheral()
         self.__stack = Stack()
-        self.__parser = Parser(instruction)
+        self.__branch = Branch()
+        self.__parser = Parser(input)
         self.__timing = TimingDiagram()
-        self.__input = instruction
+        self.__pc = Assembler()
+        self.__register = Register()
+        self.__input = input
         self.inst = {}
         self.__inst_set()
+
+        self.__rt = 0
 
     def __inst_set(self):
         for inst in [
             self.__arithmetic,
+            self.__branch,
             self.__data,
             self.__logical,
             self.__peripheral,
@@ -37,56 +48,72 @@ class Processor:
     def input(self):
         return self.__input
     
-    def exec(self, instr_list:list):
-        for inst in instr_list:
-            mnemonic, operands = inst[0], inst[1:]
+    def execute(self):
 
-            if mnemonic == 'HLT':
-                return 'HLT'
+        result = self.__parser.parse()
 
-            if mnemonic == 'CALL':
-                return mnemonic
+        if isinstance(result, Message):
+            return result
 
-            if mnemonic == 'RET':
-                return 'RET'
+        result = self.__pc.pass2()
 
-            if operands == []:
-                try:
-                    self.inst[mnemonic]()
-                except KeyError:
-                    pass
-            else:
-                try:
-                    self.inst[mnemonic](*operands)
-                except KeyError:
-                    pass
+        if isinstance(result, str):
+            return result
         
-        return 0
-            
-    def run(self):
-        struct = self.__parser.structure
-
-        if isinstance(struct, str):
-            return struct
-
-        labels = list(struct.keys())
-        prev_label = 'START'
-        output = 0
-
+        
         while True:
-            if not output:
-                label = labels.pop(0)
+            
+            
+            if self.__rt > RUNTIME:
+                return "Runtime exceeded"
 
-            stack = struct[label]
+            pc = self.__register['PC']
 
-            output = self.exec(stack)
+            inst , *code = _STACK[pc]
 
-            if output == 'HLT':
+            if inst == 'HLT':
                 break
 
-            if output:
-                if output == 'RET':
-                    label = prev_label
+            self.inst[inst]( *code )
+
+            if isinstance(self.inst[inst].__self__, Branch):
+                self.__rt += 1
+                info(f"{pc} {inst} {code}")
+                continue
+
+            else:
+                inr = INSTRUCTION[inst]['byte']
+                self.__register['PC'] = encode (decode(self.__register['PC']) + inr, 4)
+                info(f"{pc} {inst} {code}")
+                self.__rt += 1
+        
+            # try: # Handle No Return cases
+            #     pc = self.__register['PC']
+            #     inst , *code = _STACK[pc]
+            #     info(f"{pc} {inst} {code}")
+            #     return 'Infinte Loop Detected'
+            
+            # except KeyError:
+            #     break
+
+            # else:
+            #     inst , *code = _STACK[pc]
+
+            #     if inst == 'HLT':
+            #         break
+
+            #     self.inst[inst]( *code )
+
+            #     if isinstance(self.inst[inst].__self__, Branch):
+            #         continue
+
+            #     else:
+            #         inr = INSTRUCTION[inst]['byte']
+            #         self.__register['PC'] = encode (decode(self.__register['PC']) + inr, 4)
+            #         print(self.__register['PC'])
                 
-                else:
-                    prev_label, label = label, output
+            #     self.__rt += 1
+            
+            # info(f"{pc} {inst} {code}")
+        
+        self.__rt = 0
