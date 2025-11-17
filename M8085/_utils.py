916cd33ac461b9de55c166f968ccfd8cbf5d2e32
Sakar Giri <sakargiri365@gmail.com>
2025-11-17 16:38:08 +0545

    operate

--- M8085/_utils.py
+++ M8085/_utils.py
@@ -1,17 +1,20 @@
 from pathlib import Path
 import yaml
+from .logs import setup_logger, warn
+setup_logger()
 
 PATH = Path(__file__).parent
 
 with open(f"{PATH}/commands_property.yml", "r") as f:
     INSTRUCTION:dict = yaml.safe_load(f)
 
-def decode(arg:str) -> int:
+def decode(arg:str) -> int | str:
     arg = arg[:-1]  # Remove 'H' at the end
     try:
         return int(arg,16)
     except ValueError:
-        return f'Invalid Address: {arg}'
+        warn(f"Invalid Address: {arg}")
+        return f"Invalid Address: {arg}"
 
 def encode(arg:int, bit:int=2) -> str:
 
@@ -21,3 +24,12 @@ def encode(arg:int, bit:int=2) -> str:
         return '0' * (bit - len(addr)) + addr + 'H'
     
     return addr + 'H'
+
+def operate(op1: str | int, op2: str | int, flag: int = 0, bit: int = 2) -> str:
+    if isinstance(op1, str):
+        op1 = decode(op1)
+    
+    if isinstance(op2, str):
+        op2 = decode(op2)
+
+    return encode(op1 + op2 + flag, bit=bit)
